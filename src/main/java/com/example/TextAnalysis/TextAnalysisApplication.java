84678b5da7d35b4daacf0b5ca3fe68761e9aaa3e
shad-ding <970347250@qq.com>
2023-09-12 00:08:51 +0800

    "添加maven项目"

---
+++ src/main/java/com/example/TextAnalysis/TextAnalysisApplication.java
@@ -0,0 +1,11 @@
+package com.example.TextAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TextAnalysisApplication.class, args);
+    }
+}
